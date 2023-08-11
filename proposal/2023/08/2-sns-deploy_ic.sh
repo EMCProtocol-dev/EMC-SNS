77bf7c0ddc093b073ffce7bced6efc47ca02cf77
Ericyuan1029 <93896652+Ericyuan1029@users.noreply.github.com>
2023-08-11 02:29:32 -0700

    update neuron settings

--- proposal/2023/08/2-sns-deploy_ic.sh
+++ proposal/2023/08/2-sns-deploy_ic.sh
@@ -1,1 +1,1 @@
-sns deploy --network ic --init-config-file ./sns/sns_init_ic.yaml --save-to "sns/sns_canister_ids_ic.json"
+sns deploy --network ic --init-config-file ./sns/sns_init_ic.yaml --save-to "./sns/sns_canister_ids_ic.json"
